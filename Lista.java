f513402e7f0d453ab5b0b11609e1c90145782002
Romanock-17 <134869046+Romanock-17@users.noreply.github.com>
2023-09-16 08:46:40 -0500

    Add files via upload

---
+++ Lista.java
@@ -0,0 +1,172 @@
+import javax.swing.JOptionPane;
+public class Lista {
+    //Atributos
+    private Nodo punta;
+    //Métodos
+    //Constructor
+
+    public Lista() {
+        this.punta = null;
+    }
+
+    //Insertar al inicio
+    public void insertarInicio(int dato)
+    {
+        Nodo nuevo = new Nodo();
+        nuevo.setDato(dato);
+        if (this.punta == null)
+        {
+            this.punta = nuevo;
+        }
+        else
+        {
+            nuevo.setLigaD(this.punta);
+            punta.setLigaI(nuevo);
+            punta = nuevo;
+        }
+    }
+
+
+    //Mostrar lista
+
+    public void mostrarLista() {
+        Nodo aux = punta;
+        String s = "";
+        while (aux != null) {
+            System.out.print(aux.getDato() + " ");
+            s += aux.getDato() + " ";
+            aux = aux.getLigaD();
+
+        }
+        JOptionPane.showMessageDialog(null, s);
+    }
+
+    // insertar ordenado
+    // l3 = l1 + l2
+    public void ordenar() {
+        int aux;
+        Nodo P = this.punta;
+        Nodo Q = P;
+        for (P = this.punta; P != null; P = P.getLigaD()) {
+            for (Q = P.getLigaD(); Q != null; Q = Q.getLigaD()) {
+                if (P.getDato() > Q.getDato()) {
+                    aux = P.getDato();
+                    P.setDato(Q.getDato());
+                    Q.setDato(aux);
+                }
+            }
+        }
+    }
+
+    public void insertarOrdenado(int dato)
+    {
+        if(this.punta == null) return;
+        Nodo P = this.punta;
+        Nodo Q = P;
+
+        while(dato > P.getDato() && P != null){
+            Q = P;
+            P = P.getLigaD();
+        }
+        Nodo x = new Nodo(dato);
+        if(P!=Q) Q.setLigaD(x);
+        x.setLigaD(P);
+        if(P==Q) this.punta = x;
+    }
+
+    public void insertarFinal(int dato)
+    {
+
+        if (punta == null)
+        {
+            Nodo nuevo = new Nodo(dato);
+            punta = nuevo;
+        }
+        else
+        {
+            Nodo aux = punta;
+            while(aux.getLigaD() != null)
+            {
+                aux = aux.getLigaD();
+            }
+            Nodo nuevo = new Nodo(dato);
+            aux.setLigaD(nuevo);
+        }
+    }
+
+    // MARIANA HACE: Insertar ordenado, l1+l2 = l3
+    public void sumarListas(Lista A, Lista B) {
+        Nodo P = A.punta;
+        Nodo Q = B.punta;
+        while (P != null || Q != null) {
+            int d = 0;
+            if (P == null) d = Q.getDato();
+            else if (Q == null) d = P.getDato();
+            else d = P.getDato() + Q.getDato();
+            insertarFinal(d);
+            P = P.getLigaD();
+            Q = Q.getLigaD();
+        }
+
+    }
+
+    public void Reemplazar(int dato, int nuevoDato) {
+        Nodo aux = new Nodo();
+        aux=punta;
+        while (aux != null) {
+            if (aux.getDato() == dato) {
+                aux.setDato(nuevoDato);
+            }else{
+                aux = aux.getLigaD();
+            }
+        }
+    }
+
+   /* public void Busqueda(int dato) {
+    Nodo aux= new Nodo();
+    int i = 0;
+    aux=punta;
+    while(aux!=null || aux.getDato()!=dato){
+            if(aux.getDato()==dato){
+                JOptionPane.showMessageDialog(null, "El dato "+ dato+ " se encuentra en la posicion "+ i+1);
+                aux=null;;
+            }
+        }
+        aux=aux.getLigaD();
+        i++;
+    }*/
+    public void Busqueda (int Dato)
+    {
+        Nodo p = punta;
+        int i=0;
+        while (p != null || p.getDato() != Dato)
+        {
+            if (p.getDato()==Dato)
+            {
+                System.out.println("El dato "+Dato+" se encuentra en la posicion "+i);
+                JOptionPane .showMessageDialog(null, "El dato "+Dato+" se encuentra en la posicion "+i);
+                p = null;
+            }
+            p= p.getLigaD();
+            i++;
+
+        }
+    }
+
+
+   /* public void Eliminar (int dato){
+        Nodo aux=punta;
+        while(aux!=null){
+            if(aux.getDato()==dato){
+
+            }
+        }
+    }*/
+
+
+
+
+
+}
+
+
